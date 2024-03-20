472debfcdf9a336a0107b474f5d5c00073c1117a
drew-beamer <drbeamer@davidson.edu>
2024-03-19 20:59:04 -0400

    map posts from db to market page

--- next-app/src/app/market/page.tsx
+++ next-app/src/app/market/page.tsx
@@ -1,20 +1,20 @@
 import PostCard from "@/components/PostCard";
 import dbConnect from "@/lib/dbConnect";
 import Post from "@/models/Post";
+import CreatePost from "./CreatePost";
 
 export default async function MarketPage() {
   await dbConnect();
-  const testPost = await Post.findOne({});
+  const testPosts = await Post.find({});
 
   return (
     <main className="max-w-[768px] mx-auto px-4">
       <h1>Market Page</h1>
+      <CreatePost />
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-        <PostCard post={Object.freeze(testPost)} />
-        <PostCard post={Object.freeze(testPost)} />
-        <PostCard post={Object.freeze(testPost)} />
-        <PostCard post={Object.freeze(testPost)} />
-        <PostCard post={Object.freeze(testPost)} />
+        {testPosts.map((post) => (
+          <PostCard key={post._id} post={Object.freeze(post)} />
+        ))}
       </div>
     </main>
   );
