ea8514cda9c9603f384d3c18482531a1d8a1bfc1
toffyc <cjingtt@gmail.com>
2024-04-02 14:30:39 -0400

    Add google authentication and email end verification

--- next-app/src/app/api/auth/[...nextauth]/route.ts
+++ next-app/src/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,1 @@
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-
-const handler = NextAuth ({
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-        })
-    ],
- })
-
- export {handler as GET, handler as POST}
+export {GET, POST} from "@/lib/auth";
--- next-app/src/app/page.tsx
+++ next-app/src/app/page.tsx
@@ -1,12 +1,12 @@
-"use client";
-
 import { Button } from "@/components/ui/button";
 // importing necessary functions
-import { useSession, signIn, signOut } from "next-auth/react"
+import { auth } from "@/lib/auth";
+import { authenticateWithGoogle } from "./actions";
 
-export default function Home() {
+
+export default async function Home() {
   // extracting data from usesession as session
-  const { data: session } = useSession()
+  const session = await auth();
 
   // checking if sessions exists
   if (session) {
@@ -17,7 +17,7 @@ export default function Home() {
         </div>
         <p className="text-2xl mb-2">Welcome <span className="font-bold">{session.user?.name}</span>. Signed In As</p>
         <p className="font-bold mb-4">{session.user?.email}</p>
-        <Button className="bg-red-600 py-2 px-6 rounded-md" onClick={() => signOut()}>Sign out</Button>
+        <Button className="bg-red-600 py-2 px-6 rounded-md">Sign out</Button>
       </div>
     )
   }
@@ -26,7 +26,9 @@ export default function Home() {
   return (
     <div className="w-full h-screen flex flex-col justify-center items-center">
         <p className="text-2xl mb-2">Not Signed In</p>
-        <Button className="bg-blue-600 py-2 px-6 rounded-md mb-2" onClick={() => signIn('google')}>Sign in with google</Button>
+        <form action={authenticateWithGoogle}>
+          <Button type="submit" className="bg-blue-600 py-2 px-6 rounded-md mb-2">Sign in with google</Button>
+        </form>
     </div>
   )
 
