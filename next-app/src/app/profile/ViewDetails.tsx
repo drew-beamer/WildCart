6c9153cb2a81d0ed3ad3ff6d47f3bacb8a25d935
drew-beamer <drbeamer@davidson.edu>
2024-04-18 00:21:51 -0400

    fixes user score bug

--- next-app/src/app/profile/ViewDetails.tsx
+++ next-app/src/app/profile/ViewDetails.tsx
@@ -10,6 +10,7 @@ import {
 import { PostDisplay } from "../market/page";
 import { UserDisplay } from "@/components/PostCard";
 import Image from "next/image";
+import UserSchema, { User } from "@/models/User";
 
 function DescriptionDisplay({ description }: { description: string }) {
   return <p className="text-sm break-words">{description}</p>;
@@ -31,8 +32,11 @@ function TradeModeDisplay({ tradeMode }: { tradeMode: string }) {
   return <p className="text-md">{tradeMode}</p>;
 }
 
-export default function ViewDetails({ post }: Readonly<{ post: PostDisplay }>) {
+export default async function ViewDetails({
+  post,
+}: Readonly<{ post: PostDisplay }>) {
   const imageUrl = `data:image/jpeg;base64,${post.picture.toString("base64")}`;
+  const user = (await UserSchema.findById(post.seller_id)) as User;
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -51,30 +55,30 @@ export default function ViewDetails({ post }: Readonly<{ post: PostDisplay }>) {
           />
         </div>
         <Label htmlFor="Publisher">Publisher:</Label>
-        <UserDisplay sellerName={post.seller_name} />
+        <UserDisplay sellerName={post.seller_name} userScore={user.score} />
         <div className="flex flex-col max-w-md">
           <Label htmlFor="Description">Description:</Label>
           <div className="w-full">
             <DescriptionDisplay description={post.description} />
           </div>
         </div>
-        
+
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           <div>
             <Label htmlFor="Trade Mode">Trade Mode Preference:</Label>
             <TradeModeDisplay tradeMode={post.trade_mode} />
           </div>
           <div>
-          <Label htmlFor="Price">Approximate Price:</Label>
-          <PriceDisplay price={post.price} />
+            <Label htmlFor="Price">Approximate Price:</Label>
+            <PriceDisplay price={post.price} />
           </div>
           <div>
-          <Label htmlFor="Category">Category:</Label>
-          <CategoryDisplay category={post.category} />
+            <Label htmlFor="Category">Category:</Label>
+            <CategoryDisplay category={post.category} />
           </div>
           <div>
-          <Label htmlFor="Condition">Condition:</Label>
-          <ConditionDisplay condition={post.condition} />
+            <Label htmlFor="Condition">Condition:</Label>
+            <ConditionDisplay condition={post.condition} />
           </div>
         </div>
       </DialogContent>
